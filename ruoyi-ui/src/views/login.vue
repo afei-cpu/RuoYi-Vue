b894f4871effd6404f46e02a9cfe511c08348585
cloudw7 <cloudw7@163.com>
2024-07-03 16:06:35 +0800

    login.vue-init

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -7,7 +7,7 @@
           v-model="loginForm.username"
           type="text"
           auto-complete="off"
-          placeholder="账号-1"
+          placeholder="账号"
         >
           <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
         </el-input>
